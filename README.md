7c3a119df09a601e78b285f39e43bd2fe921ae23
Nick Freear <nfreear@yahoo.co.uk>
2013-04-15 21:28:10 +0100

    Update to `accessifyhtml5-marklet.js` bookmarklet Javascript

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ accessify-wiki
 ==============
 
 Working together to fix accessibility on the Web, one site at a time.
+
+
+`/browser/` contains browser plugins, bookmarklet Javascript and user Javascript (Greasemonkey).
+
+`/site/` contains the prototype site and web-service, currently mostly written in Python to be hosted on [ScraperWiki][scraper-py].
+
+
+[scraper-py]: https://scraperwiki.com/docs/python/
+[accessifyhtml5]: https://github.com/yatil/accessifyhtml5.js
+[accessify-rfc]: https://docs.google.com/document/d/1V0oTZ0m5A1iQfmftiM8VUg2N-Sm_-syXCQxzQQcD-qU/edit
+[scraper-marklet]: https://views.scraperwiki.com/run/accessify-bookmarklet
--- browser/js/accessifyhtml5-marklet.js
+++ browser/js/accessifyhtml5-marklet.js
@@ -1,72 +1,61 @@
 /*!
- Bookmarklet to apply AccessifyHTML5.js to various web sites.
+ Bookmarklet to apply AccessifyHTML5.js (WAI-ARIA fixes) to various web sites.
 
- Nick Freear, 28 March 2013.
+ Copyright Nick Freear, 28 March 2013.
 
  Self: https://dl.dropbox.com/u/3203144/wai-aria/accessifyhtml5-marklet.js
- Also: https://gist.github.com/nfreear/5254266#file-ouice-wai-aria-html
+ Gist: https://gist.github.com/nfreear/5263216#file-accessifyhtml5-marklet-js
+
 
  Bookmarklet:
 
-javascript:(function(){var sc=document.createElement('SCRIPT');sc.type='text/javascript';sc.src='https://dl.dropbox.com/u/3203144/wai-aria/accessifyhtml5-marklet.js?x='+(Math.random());document.getElementsByTagName('body')[0].appendChild(sc);document.ac5_srvurl='http://example.org'})();
+javascript:(function(){var D=document,s=D.createElement('script');s.type='text/javascript';s.src='https://dl.dropbox.com/u/3203144/wai-aria/accessifyhtml5-marklet.js?x='+(Math.random());D.body.appendChild(s)})();
+
+..;document.ac5_srvurl='http://example.org'
 */
 
 (function () {
 
   "use strict";
 
-  var map = {
-      "open.ac.uk": "https://gist.github.com/nfreear/5253410/raw/ouice-wai-aria.yaml",
-      "open.edu": "https://gist.github.com/nfreear/5253410/raw/ouice-wai-aria.yaml"
+  // Domains to be fixed and fixes would be provided by a web-service.
+  var url = {
+      ouice:  "https://gist.github.com/nfreear/5253410/raw/ouice-wai-aria.yaml",
+      google: "http://dl.dropbox.com/u/3203144/wai-aria/google-search-wai-aria.yaml",
+      baidu:  "http://dl.dropbox.com/u/3203144/wai-aria/baidu-search-wai-aria.yaml",
+
+      example: "http://example.org/my/fixes.yaml"
+    },
+    map = {
+      ".open.ac.uk": url.ouice,
+      ".open.edu":   url.ouice,
+      ".google.de":  url.google,
+      ".google.co.uk": url.google,
+      ".google.com": url.google,
+      ".baidu.com":  url.baidu,
 
       // Facebook, YouTube, Yahoo, Baidu...!
-      // "facebook.com": "https://example.org/my/facebook/fixes.yaml",
-      // ...
       // (See, http://www.alexa.com/topsites)
+      ".example.": url.example
     },
-    host = document.location.host,
+    D = document,
+    host = D.location.host,
     script = "https://raw.github.com/nfreear/accessifyhtml5.js/master/accessifyhtml5.js",
     callback = "__accessifyhtml5_bookmarklet_CB",
-    fixes_url = "https://views.scraperwiki.com/run/yaml_2_jsonp/?callback=" + callback  "&url=",
-    x,
-    re,
-    run = false;
+    fixes_url = "https://views.scraperwiki.com/run/yaml_2_jsonp/?callback=" + callback + "&url=",
+    pat, re, logp,
+    go = false;
 
 
-  function attachCallback() {
-    // Callback is global. (window["callback"])
-    window.__accessifyhtml5_bookmarklet_CB = function (fixes) {
-      log("Callback. Fixes to apply: ", fixes);
- 
-      var res = AccessifyHTML5(false, fixes);
+  logInit();
 
-      log("OK. Applied: ", res);
-    }
-  }
-
-  function addScript(src) {
-    var s = document.createElement("script");
-    s.src = src;
-    s.type = "text/javascript";
-    //s.setAttribute("async", "");
-    document.body.appendChild(s);
-  }
-
-  function log(s) {
-    if (typeof console === "object") {
-      //console.log(arguments.length > 1 ? arguments : s);
-      console.log("AccessifyHTML5", arguments);
-    }
-  }
-
-
-  for (x in map) {
-    re = new RegExp(x);
+  for (pat in map) {
+    re = new RegExp(pat.replace(/\./g, '\\.'));
     if (re.exec(host)) {
-      run = true;
-      fixes_url += map[x];
+      go = true;
+      fixes_url += encodeURIComponent(map[pat]);
 
-      log("Host matched", host, fixes_url);
+      log("Domain matched: " + pat, host, re, fixes_url);
 
       attachCallback();
       addScript(script);
@@ -74,8 +63,67 @@ javascript:(function(){var sc=document.createElement('SCRIPT');sc.type='text/jav
     }
   }
 
-  if (!run) {
-    log("No host matched", host);
+
+  if (!go) {
+    log("Sorry, no domain matched.\n", host);
+    log("› To add some fixes please visit the site.");
   }
-  
+
+
+  // ======================================================
+
+  function attachCallback() {
+    // Callback is global. (window["callback"])
+    window.__accessifyhtml5_bookmarklet_CB = function (fixes) {
+      log("Fixes found.", fixes);
+ 
+      var res = AccessifyHTML5(false, fixes);
+
+      log("OK. "+ res.ok.length +" fixes applied, "+ res.fail.length +" errors. \n", res);
+      log("› To help improve the fixes please visit   \n  ››› www.Example.org ");
+    }
+  }
+
+  function addScript(src) {
+    var s = D.createElement("script");
+    s.src = src;
+    s.type = "text/javascript";
+    s.charset = "utf-8";
+    //s.setAttribute("async", "");
+    //D.body.appendChild(s);
+    D.getElementsByTagName('body')[0].appendChild(s);
+  }
+
+
+  function log(s) {
+    // Maybe we use a multi-line title attribute ?!
+    // Was: logp.innerHTML += "&bull; " + s + "<br>\n"; //&rsaquo; //\203A
+    logp.title += "  › "+ s +" \n";
+
+    if (typeof console === "object") {
+      //console.log(arguments.length > 1 ? arguments : s);
+      console.log("AccessifyHTML5", arguments);
+    }
+  }
+
+
+  function logInit() {
+    if (!logp) {
+      logp = D.createElement('div');
+      logp.id = "AC5-log";
+      logp.title = "\n Accessify HTML5 log: \n\n";
+      logp.setAttribute("aria-live", "polite");
+      logp.setAttribute("role", "log");
+      logp.setAttribute("style",
+"display:block;position:fixed;bottom:0;right:0;width:15em;height:1.5em;"
++"font:medium sans-serif;text-align:left;background:#fcfcfc "
++"url(https://upload.wikimedia.org/wikipedia/commons/7/7f/Throbber_allbackgrounds_monochrome.gif) no-repeat right;"
++"color:#111;opacity:.8;border:3px solid #181;padding:6px;overflow-y:auto;cursor:help;");
+      //http://commons.wikimedia.org/wiki/File:Throbber_allbackgrounds_monochrome.gif
+      D.body.appendChild(logp);
+      logp.innerHTML +=
+      '<a href="http://www.example.org" style="color:navy;text-decoration:underline;">Accessify HTML5</a> ...Loading... <br>\n';
+    }
+  }
+
 })();
